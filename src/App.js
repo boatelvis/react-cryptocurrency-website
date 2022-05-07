45797b53f5717d12bf4eabd5bdbfa4eafd397744
Elvis Boateng <eboat086@gmail.com>
2022-05-07 22:00:28 +0000

    added the home component and api headers

--- src/App.js
+++ src/App.js
@@ -32,9 +32,17 @@ const App = () => {
                     </Switch>
                 </div>
             </Layout>
-        </div>
-        <div className="footer">
-
+            <div className="footer">
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                    CryptoHub <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
         </div>
     </div>
   );
