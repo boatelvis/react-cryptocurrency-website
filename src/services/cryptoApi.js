27c7078d1f66c6c9f7cd58d94759840bfe544ead
Elvis Boateng <eboat086@gmail.com>
2022-05-12 05:02:31 +0000

    modified the components and app files

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '6c8bc481e9mshfe83b877b891299p14807djsnc6fc3875cd91'
+   'X-RapidAPI-Key': '6c8bc481e9mshfe83b877b891299p14807djsnc6fc3875cd91'   
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
